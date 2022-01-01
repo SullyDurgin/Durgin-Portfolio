03018624a2823c78ca149c47ad84c1f1317a211f
SullyDurgin <katsdurgin@gmail.com>
2022-01-01 14:40:44 -0500

    added images

---
+++ src/data.js
@@ -0,0 +1,53 @@
+export const projects = [
+	{
+		title: 'Hungry Humans',
+		subtitle: 'Express, JavaScript, MongoDB and Mongoose',
+		description:
+			'This is an app that allows the user to create, read, update & delete recipes. As a user you can also look at other people who have signed in and explore their profiles. This app also allows the user to write & read reviews.',
+		image: './project-1.png',
+		link: 'https://hungry-humans.herokuapp.com/',
+	},
+	{
+		title: 'Classic Snake',
+		subtitle: 'HTML, CSS and JavaScript',
+		description:
+			'Snake is the common name for a video game concept where the player maneuvers a line which grows in length, with the line itself being a primary obstacle.',
+		image: './project-2.png',
+		link: 'https://sullys-snake.netlify.app/',
+	},
+	{
+		title: 'Spooky Forum',
+		subtitle: 'Django, Python and JavaScript',
+		description:
+			'I wanted a place where people who love stories of the strange and unusual could get together and share their experiences. That or share some stories that really give them the creeps.',
+		image: './project-3.png',
+		link: ' https://thestrangeandunusual.herokuapp.com/',
+	},
+	{
+		title: 'LFG Social App',
+		subtitle: 'React and Express.js',
+		description:
+			'LFG is a community-based web app for sharing your favorite games and nerdy opinions online and finding folks near you who agree. Search for new game stores, restaurants or bars to meet up, and local events to make the most of your local gaming community!',
+		image: './project-4.png',
+		link: 'https://lfgpls.herokuapp.com/',
+	},
+]
+
+export const testimonials = [
+	// {
+	// 	quote:
+	// 		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.',
+	// 	image: 'https://randomuser.me/api/portraits/men/1.jpg',
+	// 	name: 'Human 1',
+	// 	company: 'Company 1',
+	// },
+	// {
+	// 	quote:
+	// 		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.',
+	// 	image: 'https://randomuser.me/api/portraits/men/94.jpg',
+	// 	name: 'Human 2',
+	// 	company: 'Company 2',
+	// },
+]
+
+export const skills = ['JavaScript', 'React', 'HTML', 'CSS', 'Node', ,]
