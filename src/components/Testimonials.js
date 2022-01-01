6f42ab11b1d74fcd5bcb968db8551b7716bf4d51
SullyDurgin <katsdurgin@gmail.com>
2022-01-01 18:19:47 -0500

    changes in style

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -7,7 +7,6 @@ export default function Testimonials() {
 		<section id='testimonials'>
 			<div className='container px-5 py-10 mx-auto text-center'>
 				<h1 className='sm:text-4xl text-3xl font-medium title-font text-white mb-12'>
-					Client Testimonials
 				</h1>
 				<div className='flex flex-wrap m-4'>
 					{testimonials.map((testimonial) => (
