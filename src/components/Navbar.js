6f13c3829c56c3b23938392178e4de5453e04a79
SullyDurgin <katsdurgin@gmail.com>
2022-01-05 16:25:08 -0500

    change shadow click

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import { ArrowRightIcon } from '@heroicons/react/solid'
 import React from 'react'
 
 export default function Navbar() {
+	
 	return (
 		<header className='bg-gray-800 md:sticky top-0 z-10'>
 			<div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
@@ -11,6 +12,7 @@ export default function Navbar() {
 					</a>
 				</a>
 				<nav className='md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center'>
+					
 					<a href='#projects' className='mr-5 hover:text-white'>
 						Past Work
 					</a>
